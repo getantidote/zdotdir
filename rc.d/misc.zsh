3d24ba2e99d7a7ff86e877a77b117c3b62a888b1
mattmc3 <mattmc3@gmail.com>
2023-01-28 16:53:40 -0500

    Refactor rc.d

---
+++ rc.d/misc.zsh
@@ -0,0 +1,30 @@
+#
+# misc - Set general Zsh config options here, or change plugin settings.
+#
+
+#
+# Options
+#
+
+# Be quiet!
+setopt NO_BEEP
+setopt NO_HIST_BEEP
+
+#
+# Magic-Enter
+#
+
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/magic-enter
+if [[ "$OSTYPE" == darwin* ]]; then
+  MAGIC_ENTER_OTHER_COMMAND='ls -G'
+else
+  MAGIC_ENTER_OTHER_COMMAND='ls --color=auto'
+fi
+MAGIC_ENTER_GIT_COMMAND="$MAGIC_ENTER_OTHER_COMMAND && git status -sb"
+
+#
+# Zsh-Utils
+#
+
+# The belek/zsh-utils completion plugin also introduces compstyles. Let's use that!
+(( ! $+functions[compstyle_zshzoo_setup] )) || compstyle_zshzoo_setup
