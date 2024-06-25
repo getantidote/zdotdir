9850260913b9fe75f575131e9c8f300d5fd024de
mattmc3 <mattmc3@gmail.com>
2024-06-25 13:15:55 -0400

    Set more history variables

--- conf.d/history.zsh
+++ conf.d/history.zsh
@@ -2,5 +2,7 @@
 # history: Set better history values
 #
 
+HISTSIZE=100000
+SAVEHIST=100000
 HISTFILE=${XDG_DATA_HOME:-$HOME/.local/share}/zsh/history
 [[ -d ${HISTFILE:h} ]] || mkdir -p ${HISTFILE:h}
